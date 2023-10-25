ab56edd95ebe1e60a6634f09a74fd5a9af1c67da
jeremiahquiambao <quiambao.jeremiah@auf.edu.ph>
2023-10-25 11:50:46 +0800

    2nd commit as first repo was corrupted

---
+++ App6/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OpenPage1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+        private async void OpenPage2(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page2());
+        }
+    }
+}
